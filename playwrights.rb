2c5e9b552b429dcec2cac64dcef444f0be24e07f
Kevin Cornea <kpcornea@gmail.com>
2018-12-25 21:05:27 -0800

    Add Playwright#update

--- playwrights.rb
+++ playwrights.rb
@@ -37,4 +37,16 @@ class Playwright
     @id = PlayDBConnection.instance.last_insert_row_id
   end
 
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
 end
